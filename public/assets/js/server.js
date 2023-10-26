169e65b39b6f67f2ce074bd069cbabfbe4cef81f
Estevan <egonz94@gmail.com>
2023-10-26 18:26:22 -0500

    Added /notes.html GET route

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const PORT = 3001;
 const app = express();
 const notes = require('../../../db/db.json')
 
 app.use(express.json());
+
+app.get('/notes', (req, res) => {
+  console.info("returning notes.html");
+  res.sendFile(path.join(__dirname, '../../../public/notes.html'));
+
+})
 
 app.get('/api/notes', (req, res) => {
     // Log our request to the terminal
